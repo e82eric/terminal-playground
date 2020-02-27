e82eric/terminal-playground

commit 27d7f2f01fcc9ddf7ea484957401151b2f18598b

Throttle SetEvent call in render thread (#3511)
Reduce unnecessary SetEvent CPU overhead by using `std::atomic_flag` to avoid setting kernel events when the painting thread is already awake and running.

src/renderer/base/thread.cpp
@@ -17,6 +17,8 @@ RenderThread::RenderThread() :
     _fKeepRunning(true),
     _hPaintEnabledEvent(nullptr)
 {
+    _fNextFrameRequested.clear();
+    _fPainting.clear();
 }
 
 RenderThread::~RenderThread()
@@ -158,9 +160,20 @@ DWORD WINAPI RenderThread::_ThreadProc()
     while (_fKeepRunning)
     {
         WaitForSingleObject(_hPaintEnabledEvent, INFINITE);
-        WaitForSingleObject(_hEvent, INFINITE);
+
+        // Skip waiting if next frame is requested.
+        if (_fNextFrameRequested.test_and_set(std::memory_order_relaxed))
+        {
+            _fNextFrameRequested.clear(std::memory_order_relaxed);
+        }
+        else
+        {
+            WaitForSingleObject(_hEvent, INFINITE);
+        }
 
         ResetEvent(_hPaintCompletedEvent);
+
+        _fPainting.test_and_set(std::memory_order_acquire);
 
         LOG_IF_FAILED(_pRenderer->PaintFrame());
 
@@ -171,6 +184,8 @@ DWORD WINAPI RenderThread::_ThreadProc()
         {
             Sleep(s_FrameLimitMilliseconds);
         }
+
+        _fPainting.clear(std::memory_order_release);
     }
 
     return S_OK;
@@ -178,6 +193,14 @@ DWORD WINAPI RenderThread::_ThreadProc()
 
 void RenderThread::NotifyPaint()
 {
+    // If we are currently painting a frame, set _fNextFrameRequested flag
+    // to indicate we want to paint next frame immediately.
+    if (_fPainting.test_and_set(std::memory_order_acquire))
+    {
+        _fNextFrameRequested.test_and_set(std::memory_order_relaxed);
+        return;
+    }
+
     SetEvent(_hEvent);
 }
 

src/renderer/base/thread.hpp
@@ -47,5 +47,7 @@ namespace Microsoft::Console::Render
         IRenderer* _pRenderer; // Non-ownership pointer
 
         bool _fKeepRunning;
+        std::atomic_flag _fNextFrameRequested;
+        std::atomic_flag _fPainting;
     };
 }

commit 065676555aa1eafd0e47a8f17cf9f4fbcb268667

Fix RenderThread's notify mechanism (#4698)
## Summary of the Pull Request

Fix a bug where the `Renderer::PaintFrame` method:
1. is not called until the next `RenderThread::NotifyThread` call but needs to be called because there the terminal was updated (theoretical bug)
2. is called twice but needs to be called only once (verified bug)

## References

The bug was introduced by #3511.

## PR Checklist
* [x] CLA signed. If not, go over [here](https://cla.opensource.microsoft.com/microsoft/Terminal) and sign the CLA

## Detailed Description of the Pull Request / Additional comments

### Before

#### First bug

In the original code, `_fNextFrameRequested` is set to `true` in render thread because `std::atomic_flag::test_and_set` is called.

This is wrong because it means that the render thread will render the terminal again even if there is no change after the last render.

I think the the goal was to load the boolean value for `_fNextFrameRequested` to check whether the thread should sleep or not.

The problem is that there is no method on `std::atomic_flag` to load its boolean value. I guess what happened was that the "solution" that was found was to use `std::atomic_flag::test_and_set`, followed by `std::atomic_flag::clear` if the value was `false` originally (if `std::atomic_flag::test_and_set` returned `false`) to restore the original value. I guess that this was believed to be equivalent to just a simple load, without doing any change to the value because it restores it at the end.

But it's not: this is dangerous because if the value is changed to `true` between the call to `std::atomic_flag::test_and_set` and the call to `std::atomic_flag::clear`, then the value ends up being `false` at the end which is wrong because we don't want to change it! And if that value ends up being `false`, it means that we miss a render because we will wait on `_hEvent` during the next iteration on the render thread.

src/renderer/base/thread.cpp
@@ -15,10 +15,10 @@ RenderThread::RenderThread() :
     _hEvent(nullptr),
     _hPaintCompletedEvent(nullptr),
     _fKeepRunning(true),
-    _hPaintEnabledEvent(nullptr)
+    _hPaintEnabledEvent(nullptr),
+    _fNextFrameRequested(false),
+    _fWaiting(false)
 {
-    _fNextFrameRequested.clear();
-    _fPainting.clear();
 }
 
 RenderThread::~RenderThread()
@@ -161,19 +161,44 @@ DWORD WINAPI RenderThread::_ThreadProc()
     {
         WaitForSingleObject(_hPaintEnabledEvent, INFINITE);
 
-        // Skip waiting if next frame is requested.
-        if (_fNextFrameRequested.test_and_set(std::memory_order_relaxed))
+        if (!_fNextFrameRequested.exchange(false))
         {
-            _fNextFrameRequested.clear(std::memory_order_relaxed);
-        }
-        else
-        {
-            WaitForSingleObject(_hEvent, INFINITE);
+            // <--
+            // If `NotifyPaint` is called at this point, then it will not
+            // set the event because `_fWaiting` is not `true` yet so we have
+            // to check again below.
+
+            _fWaiting.store(true);
+
+            // check again now (see comment above)
+            if (!_fNextFrameRequested.exchange(false))
+            {
+                // Wait until a next frame is requested.
+                WaitForSingleObject(_hEvent, INFINITE);
+            }
+
+            // <--
+            // If `NotifyPaint` is called at this point, then it _will_ set
+            // the event because `_fWaiting` is `true`, but we're not waiting
+            // anymore!
+            // This can probably happen quite often: imagine a scenario where
+            // we are waiting, and the terminal calls `NotifyPaint` twice
+            // very quickly.
+            // In that case, both calls might end up calling `SetEvent`. The
+            // first one will resume this thread and the second one will
+            // `SetEvent` the event. So the next time we wait, the event will
+            // already be set and we won't actually wait.
+            // Because it can happen often, and because rendering is an
+            // expensive operation, we should reset the event to not render
+            // again if nothing changed.
+
+            _fWaiting.store(false);
+
+            // see comment above
+            ResetEvent(_hEvent);
         }
 
         ResetEvent(_hPaintCompletedEvent);
-
-        _fPainting.test_and_set(std::memory_order_acquire);
 
         LOG_IF_FAILED(_pRenderer->PaintFrame());
 
@@ -184,8 +209,6 @@ DWORD WINAPI RenderThread::_ThreadProc()
         {
             Sleep(s_FrameLimitMilliseconds);
         }
-
-        _fPainting.clear(std::memory_order_release);
     }
 
     return S_OK;
@@ -193,15 +216,14 @@ DWORD WINAPI RenderThread::_ThreadProc()
 
 void RenderThread::NotifyPaint()
 {
-    // If we are currently painting a frame, set _fNextFrameRequested flag
-    // to indicate we want to paint next frame immediately.
-    if (_fPainting.test_and_set(std::memory_order_acquire))
+    if (_fWaiting.load())
     {
-        _fNextFrameRequested.test_and_set(std::memory_order_relaxed);
-        return;
+        SetEvent(_hEvent);
     }
-
-    SetEvent(_hEvent);
+    else
+    {
+        _fNextFrameRequested.store(true);
+    }
 }
 
 void RenderThread::EnablePainting()

src/renderer/base/thread.hpp
@@ -47,7 +47,7 @@ namespace Microsoft::Console::Render
         IRenderer* _pRenderer; // Non-ownership pointer
 
         bool _fKeepRunning;
-        std::atomic_flag _fNextFrameRequested;
-        std::atomic_flag _fPainting;
+        std::atomic<bool> _fNextFrameRequested;
+        std::atomic<bool> _fWaiting;
     };
 }