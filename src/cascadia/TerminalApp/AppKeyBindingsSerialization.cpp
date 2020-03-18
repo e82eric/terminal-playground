e82eric/terminal-playground

commit 067e9fc73b35ed4dcad6886b5764f6ae4a7a071c

Add warning messages for bad keybindings (#4746)
## Summary of the Pull Request

Adds warning messages for a pair of keybindings-related scenarios. This covers the following two bugs:
* #4239 - If the user has supplied more than one key chord in their `"keys"` array.
* #3522 - If a keybinding has a _required_ argument, then we'll display a message to the user
  - currently, the only required parameter is the `direction` parameter for both `resizePane` and `moveFocus`

## References

When we get to #1334, we'll want to remove the `TooManyKeysForChord` warning.

## PR Checklist
* [x] Closes #4239
* [x] Closes #3522
* [x] I work here
* [x] Tests added/passed
* [n/a] Requires documentation to be updated

![image](https://user-images.githubusercontent.com/18356694/75593132-f18ec700-5a49-11ea-9d26-6acd0d28b0b7.png)


## Validation Steps Performed
Tested manually, added tests.

src/cascadia/TerminalApp/AppKeyBindingsSerialization.cpp
@@ -146,6 +146,9 @@ static const std::map<std::string_view, ShortcutAction, std::less<>> commandName
     { FindKey, ShortcutAction::Find },
 };
 
+using ParseResult = std::tuple<IActionArgs, std::vector<TerminalApp::SettingsLoadWarnings>>;
+using ParseActionFunction = std::function<ParseResult(const Json::Value&)>;
+
 // Function Description:
 // - Creates a function that can be used to generate a SplitPaneArgs for the
 //   legacy Split[SplitState] actions. These actions don't accept args from
@@ -157,12 +160,12 @@ static const std::map<std::string_view, ShortcutAction, std::less<>> commandName
 // Return Value:
 // - A function that can be used to "parse" json into one of the legacy
 //   Split[SplitState] args.
-std::function<IActionArgs(const Json::Value&)> LegacyParseSplitPaneArgs(SplitState style)
+ParseActionFunction LegacyParseSplitPaneArgs(SplitState style)
 {
-    auto pfn = [style](const Json::Value & /*value*/) -> IActionArgs {
+    auto pfn = [style](const Json::Value & /*value*/) -> ParseResult {
         auto args = winrt::make_self<winrt::TerminalApp::implementation::SplitPaneArgs>();
         args->SplitStyle(style);
-        return *args;
+        return { *args, {} };
     };
     return pfn;
 }
@@ -178,12 +181,12 @@ std::function<IActionArgs(const Json::Value&)> LegacyParseSplitPaneArgs(SplitSta
 // Return Value:
 // - A function that can be used to "parse" json into one of the legacy
 //   MoveFocus[Direction] args.
-std::function<IActionArgs(const Json::Value&)> LegacyParseMoveFocusArgs(Direction direction)
+ParseActionFunction LegacyParseMoveFocusArgs(Direction direction)
 {
-    auto pfn = [direction](const Json::Value & /*value*/) -> IActionArgs {
+    auto pfn = [direction](const Json::Value & /*value*/) -> ParseResult {
         auto args = winrt::make_self<winrt::TerminalApp::implementation::MoveFocusArgs>();
         args->Direction(direction);
-        return *args;
+        return { *args, {} };
     };
     return pfn;
 }
@@ -199,12 +202,12 @@ std::function<IActionArgs(const Json::Value&)> LegacyParseMoveFocusArgs(Directio
 // Return Value:
 // - A function that can be used to "parse" json into one of the legacy
 //   ResizePane[Direction] args.
-std::function<IActionArgs(const Json::Value&)> LegacyParseResizePaneArgs(Direction direction)
+ParseActionFunction LegacyParseResizePaneArgs(Direction direction)
 {
-    auto pfn = [direction](const Json::Value & /*value*/) -> IActionArgs {
+    auto pfn = [direction](const Json::Value & /*value*/) -> ParseResult {
         auto args = winrt::make_self<winrt::TerminalApp::implementation::ResizePaneArgs>();
         args->Direction(direction);
-        return *args;
+        return { *args, {} };
     };
     return pfn;
 }
@@ -220,14 +223,14 @@ std::function<IActionArgs(const Json::Value&)> LegacyParseResizePaneArgs(Directi
 // Return Value:
 // - A function that can be used to "parse" json into one of the legacy
 //   NewTabWithProfile[Index] args.
-std::function<IActionArgs(const Json::Value&)> LegacyParseNewTabWithProfileArgs(int index)
+ParseActionFunction LegacyParseNewTabWithProfileArgs(int index)
 {
-    auto pfn = [index](const Json::Value & /*value*/) -> IActionArgs {
+    auto pfn = [index](const Json::Value & /*value*/) -> ParseResult {
         auto args = winrt::make_self<winrt::TerminalApp::implementation::NewTabArgs>();
         auto newTerminalArgs = winrt::make_self<winrt::TerminalApp::implementation::NewTerminalArgs>();
         newTerminalArgs->ProfileIndex(index);
         args->TerminalArgs(*newTerminalArgs);
-        return *args;
+        return { *args, {} };
     };
     return pfn;
 }
@@ -243,12 +246,12 @@ std::function<IActionArgs(const Json::Value&)> LegacyParseNewTabWithProfileArgs(
 // Return Value:
 // - A function that can be used to "parse" json into one of the legacy
 //   SwitchToTab[Index] args.
-std::function<IActionArgs(const Json::Value&)> LegacyParseSwitchToTabArgs(int index)
+ParseActionFunction LegacyParseSwitchToTabArgs(int index)
 {
-    auto pfn = [index](const Json::Value & /*value*/) -> IActionArgs {
+    auto pfn = [index](const Json::Value & /*value*/) -> ParseResult {
         auto args = winrt::make_self<winrt::TerminalApp::implementation::SwitchToTabArgs>();
         args->TabIndex(index);
-        return *args;
+        return { *args, {} };
     };
     return pfn;
 }
@@ -261,11 +264,11 @@ std::function<IActionArgs(const Json::Value&)> LegacyParseSwitchToTabArgs(int in
 // - direction: the direction to create the parse function for.
 // Return Value:
 // - A CopyTextArgs with TrimWhitespace set to true, to emulate "CopyTextWithoutNewlines".
-IActionArgs LegacyParseCopyTextWithoutNewlinesArgs(const Json::Value& /*json*/)
+ParseResult LegacyParseCopyTextWithoutNewlinesArgs(const Json::Value& /*json*/)
 {
     auto args = winrt::make_self<winrt::TerminalApp::implementation::CopyTextArgs>();
     args->TrimWhitespace(false);
-    return *args;
+    return { *args, {} };
 };
 
 // Function Description:
@@ -276,12 +279,12 @@ IActionArgs LegacyParseCopyTextWithoutNewlinesArgs(const Json::Value& /*json*/)
 // - delta: the font size delta to create the parse function for.
 // Return Value:
 // - A function that can be used to "parse" json into an AdjustFontSizeArgs.
-std::function<IActionArgs(const Json::Value&)> LegacyParseAdjustFontSizeArgs(int delta)
+ParseActionFunction LegacyParseAdjustFontSizeArgs(int delta)
 {
-    auto pfn = [delta](const Json::Value & /*value*/) -> IActionArgs {
+    auto pfn = [delta](const Json::Value & /*value*/) -> ParseResult {
         auto args = winrt::make_self<winrt::TerminalApp::implementation::AdjustFontSizeArgs>();
         args->Delta(delta);
-        return *args;
+        return { *args, {} };
     };
     return pfn;
 }
@@ -291,7 +294,7 @@ std::function<IActionArgs(const Json::Value&)> LegacyParseAdjustFontSizeArgs(int
 // from json. Each type of IActionArgs that can accept arbitrary args should be
 // placed into this map, with the corresponding deserializer function as the
 // value.
-static const std::map<ShortcutAction, std::function<IActionArgs(const Json::Value&)>, std::less<>> argParsers{
+static const std::map<ShortcutAction, ParseActionFunction, std::less<>> argParsers{
     { ShortcutAction::CopyText, winrt::TerminalApp::implementation::CopyTextArgs::FromJson },
     { ShortcutAction::CopyTextWithoutNewlines, LegacyParseCopyTextWithoutNewlinesArgs },
 
@@ -427,8 +430,14 @@ static ShortcutAction GetActionFromString(const std::string_view actionString)
 //   `"unbound"`, then we'll clear the keybinding from the existing keybindings.
 // Arguments:
 // - json: and array of JsonObject's to deserialize into our _keyShortcuts mapping.
-void winrt::TerminalApp::implementation::AppKeyBindings::LayerJson(const Json::Value& json)
+std::vector<::TerminalApp::SettingsLoadWarnings> winrt::TerminalApp::implementation::AppKeyBindings::LayerJson(const Json::Value& json)
 {
+    // It's possible that the user provided keybindings have some warnings in
+    // them - problems that we should alert the user to, but we can recover
+    // from. Most of these warnings cannot be detected later in the Validate
+    // settings phase, so we'll collect them now.
+    std::vector<::TerminalApp::SettingsLoadWarnings> warnings;
+
     for (const auto& value : json)
     {
         if (!value.isObject())
@@ -443,6 +452,15 @@ void winrt::TerminalApp::implementation::AppKeyBindings::LayerJson(const Json::V
         {
             const auto validString = keys.isString();
             const auto validArray = keys.isArray() && keys.size() == 1;
+
+            // GH#4239 - If the user provided more than one key
+            // chord to a "keys" array, warn the user here.
+            // TODO: GH#1334 - remove this check.
+            if (keys.isArray() && keys.size() > 1)
+            {
+                warnings.push_back(::TerminalApp::SettingsLoadWarnings::TooManyKeysForChord);
+            }
+
             if (!validString && !validArray)
             {
                 continue;
@@ -486,14 +504,21 @@ void winrt::TerminalApp::implementation::AppKeyBindings::LayerJson(const Json::V
             // does, we'll try to deserialize any "args" that were provided with
             // the binding.
             IActionArgs args{ nullptr };
+            std::vector<::TerminalApp::SettingsLoadWarnings> parseWarnings;
             const auto deserializersIter = argParsers.find(action);
             if (deserializersIter != argParsers.end())
             {
                 auto pfn = deserializersIter->second;
                 if (pfn)
                 {
-                    args = pfn(argsVal);
+                    std::tie(args, parseWarnings) = pfn(argsVal);
                 }
+            }
+            warnings.insert(warnings.end(), parseWarnings.begin(), parseWarnings.end());
+
+            if (args == nullptr)
+            {
+                continue;
             }
 
             // Try parsing the chord
@@ -523,4 +548,6 @@ void winrt::TerminalApp::implementation::AppKeyBindings::LayerJson(const Json::V
             }
         }
     }
+
+    return warnings;
 }

src/cascadia/LocalTests_TerminalApp/SettingsTests.cpp
@@ -70,6 +70,8 @@ namespace TerminalAppLocalTests
 
         TEST_METHOD(TestTerminalArgsForBinding);
 
+        TEST_METHOD(ValidateKeybindingsWarnings);
+
         TEST_CLASS_SETUP(ClassSetup)
         {
             InitializeJsonReader();
@@ -2089,4 +2091,48 @@ namespace TerminalAppLocalTests
             VERIFY_ARE_EQUAL(2, termSettings.HistorySize());
         }
     }
+
+    void SettingsTests::ValidateKeybindingsWarnings()
+    {
+        const std::string badSettings{ R"(
+        {
+            "globals": {
+                "defaultProfile": "{6239a42c-2222-49a3-80bd-e8fdd045185c}"
+            },
+            "profiles": [
+                {
+                    "name" : "profile0",
+                    "guid": "{6239a42c-2222-49a3-80bd-e8fdd045185c}"
+                },
+                {
+                    "name" : "profile1",
+                    "guid": "{6239a42c-3333-49a3-80bd-e8fdd045185c}"
+                }
+            ],
+            "keybindings": [
+                { "command": { "action": "splitPane", "split":"auto" }, "keys": [ "ctrl+alt+t", "ctrl+a" ] },
+                { "command": { "action": "moveFocus" }, "keys": [ "ctrl+a" ] },
+                { "command": { "action": "resizePane" }, "keys": [ "ctrl+b" ] }
+            ]
+        })" };
+
+        const auto settingsObject = VerifyParseSucceeded(badSettings);
+        auto settings = CascadiaSettings::FromJson(settingsObject);
+
+        VERIFY_ARE_EQUAL(0u, settings->_globals._keybindings->_keyShortcuts.size());
+
+        VERIFY_ARE_EQUAL(3u, settings->_globals._keybindingsWarnings.size());
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::TooManyKeysForChord, settings->_globals._keybindingsWarnings.at(0));
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::MissingRequiredParameter, settings->_globals._keybindingsWarnings.at(1));
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::MissingRequiredParameter, settings->_globals._keybindingsWarnings.at(2));
+
+        settings->_ValidateKeybindings();
+
+        VERIFY_ARE_EQUAL(4u, settings->_warnings.size());
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::AtLeastOneKeybindingWarning, settings->_warnings.at(0));
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::TooManyKeysForChord, settings->_warnings.at(1));
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::MissingRequiredParameter, settings->_warnings.at(2));
+        VERIFY_ARE_EQUAL(::TerminalApp::SettingsLoadWarnings::MissingRequiredParameter, settings->_warnings.at(3));
+    }
+
 }

src/cascadia/TerminalApp/ActionArgs.h
@@ -17,6 +17,7 @@
 
 #include "../../cascadia/inc/cppwinrt_utils.h"
 #include "Utils.h"
+#include "TerminalWarnings.h"
 
 // Notes on defining ActionArgs and ActionEventArgs:
 // * All properties specific to an action should be defined as an ActionArgs
@@ -26,6 +27,8 @@
 
 namespace winrt::TerminalApp::implementation
 {
+    using FromJsonResult = std::tuple<winrt::TerminalApp::IActionArgs, std::vector<::TerminalApp::SettingsLoadWarnings>>;
+
     struct ActionEventArgs : public ActionEventArgsT<ActionEventArgs>
     {
         ActionEventArgs() = default;
@@ -104,7 +107,7 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<CopyTextArgs>();
@@ -112,7 +115,7 @@ namespace winrt::TerminalApp::implementation
             {
                 args->_TrimWhitespace = trimWhitespace.asBool();
             }
-            return *args;
+            return { *args, {} };
         }
     };
 
@@ -131,12 +134,12 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<NewTabArgs>();
             args->_TerminalArgs = NewTerminalArgs::FromJson(json);
-            return *args;
+            return { *args, {} };
         }
     };
 
@@ -157,7 +160,7 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<SwitchToTabArgs>();
@@ -165,7 +168,7 @@ namespace winrt::TerminalApp::implementation
             {
                 args->_TabIndex = tabIndex.asUInt();
             }
-            return *args;
+            return { *args, {} };
         }
     };
 
@@ -221,7 +224,7 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<ResizePaneArgs>();
@@ -229,7 +232,14 @@ namespace winrt::TerminalApp::implementation
             {
                 args->_Direction = ParseDirection(directionString.asString());
             }
-            return *args;
+            if (args->_Direction == TerminalApp::Direction::None)
+            {
+                return { nullptr, { ::TerminalApp::SettingsLoadWarnings::MissingRequiredParameter } };
+            }
+            else
+            {
+                return { *args, {} };
+            }
         }
     };
 
@@ -250,7 +260,7 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<MoveFocusArgs>();
@@ -258,7 +268,14 @@ namespace winrt::TerminalApp::implementation
             {
                 args->_Direction = ParseDirection(directionString.asString());
             }
-            return *args;
+            if (args->_Direction == TerminalApp::Direction::None)
+            {
+                return { nullptr, { ::TerminalApp::SettingsLoadWarnings::MissingRequiredParameter } };
+            }
+            else
+            {
+                return { *args, {} };
+            }
         }
     };
 
@@ -279,7 +296,7 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<AdjustFontSizeArgs>();
@@ -287,7 +304,7 @@ namespace winrt::TerminalApp::implementation
             {
                 args->_Delta = jsonDelta.asInt();
             }
-            return *args;
+            return { *args, {} };
         }
     };
 
@@ -333,7 +350,7 @@ namespace winrt::TerminalApp::implementation
             }
             return false;
         };
-        static winrt::TerminalApp::IActionArgs FromJson(const Json::Value& json)
+        static FromJsonResult FromJson(const Json::Value& json)
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<SplitPaneArgs>();
@@ -342,7 +359,7 @@ namespace winrt::TerminalApp::implementation
             {
                 args->_SplitStyle = ParseSplitState(jsonStyle.asString());
             }
-            return *args;
+            return { *args, {} };
         }
     };
 }

src/cascadia/TerminalApp/AppKeyBindings.h
@@ -53,7 +53,7 @@ namespace winrt::TerminalApp::implementation
         static Windows::System::VirtualKeyModifiers ConvertVKModifiers(winrt::Microsoft::Terminal::Settings::KeyModifiers modifiers);
 
         // Defined in AppKeyBindingsSerialization.cpp
-        void LayerJson(const Json::Value& json);
+        std::vector<::TerminalApp::SettingsLoadWarnings> LayerJson(const Json::Value& json);
         Json::Value ToJson();
 
         void SetDispatch(const winrt::TerminalApp::ShortcutActionDispatch& dispatch);

src/cascadia/TerminalApp/AppLogic.cpp
@@ -27,14 +27,17 @@ namespace winrt
 // !!! IMPORTANT !!!
 // Make sure that these keys are in the same order as the
 // SettingsLoadWarnings/Errors enum is!
-static const std::array<std::wstring_view, 5> settingsLoadWarningsLabels {
+static const std::array<std::wstring_view, static_cast<uint32_t>(SettingsLoadWarnings::WARNINGS_SIZE)> settingsLoadWarningsLabels {
     USES_RESOURCE(L"MissingDefaultProfileText"),
     USES_RESOURCE(L"DuplicateProfileText"),
     USES_RESOURCE(L"UnknownColorSchemeText"),
     USES_RESOURCE(L"InvalidBackgroundImage"),
-    USES_RESOURCE(L"InvalidIcon")
+    USES_RESOURCE(L"InvalidIcon"),
+    USES_RESOURCE(L"AtLeastOneKeybindingWarning"),
+    USES_RESOURCE(L"TooManyKeysForChord"),
+    USES_RESOURCE(L"MissingRequiredParameter")
 };
-static const std::array<std::wstring_view, 2> settingsLoadErrorsLabels {
+static const std::array<std::wstring_view, static_cast<uint32_t>(SettingsLoadErrors::ERRORS_SIZE)> settingsLoadErrorsLabels {
     USES_RESOURCE(L"NoProfilesText"),
     USES_RESOURCE(L"AllProfilesHiddenText")
 };

src/cascadia/TerminalApp/CascadiaSettings.cpp
@@ -206,9 +206,11 @@ void CascadiaSettings::_ValidateSettings()
     // there's _NO_ keys bound to any actions. That's highly irregular, and
     // likely an indication of an error somehow.
 
-    // TODO:GH#3522 With variable args to keybindings, it's possible that a user
+    // GH#3522 - With variable args to keybindings, it's possible that a user
     // set a keybinding without all the required args for an action. Display a
     // warning if an action didn't have a required arg.
+    // This will also catch other keybinding warnings, like from GH#4239
+    _ValidateKeybindings();
 }
 
 // Method Description:
@@ -650,4 +652,24 @@ GUID CascadiaSettings::_GetProfileForIndex(std::optional<int> index) const
         profileGuid = _globals.GetDefaultProfile();
     }
     return profileGuid;
+}
+
+// Method Description:
+// - If there were any warnings we generated while parsing the user's
+//   keybindings, add them to the list of warnings here. If there were warnings
+//   generated in this way, we'll add a AtLeastOneKeybindingWarning, which will
+//   act as a header for the other warnings
+// Arguments:
+// - <none>
+// Return Value:
+// - <none>
+void CascadiaSettings::_ValidateKeybindings()
+{
+    auto keybindingWarnings = _globals.GetKeybindingsWarnings();
+
+    if (!keybindingWarnings.empty())
+    {
+        _warnings.push_back(::TerminalApp::SettingsLoadWarnings::AtLeastOneKeybindingWarning);
+        _warnings.insert(_warnings.end(), keybindingWarnings.begin(), keybindingWarnings.end());
+    }
 }

src/cascadia/TerminalApp/CascadiaSettings.h
@@ -116,6 +116,7 @@ private:
     void _RemoveHiddenProfiles();
     void _ValidateAllSchemesExist();
     void _ValidateMediaResources();
+    void _ValidateKeybindings();
 
     friend class TerminalAppLocalTests::SettingsTests;
     friend class TerminalAppLocalTests::ProfileTests;

src/cascadia/TerminalApp/GlobalAppSettings.cpp
@@ -43,6 +43,7 @@ static constexpr std::wstring_view SystemThemeValue{ L"system" };
 
 GlobalAppSettings::GlobalAppSettings() :
     _keybindings{ winrt::make_self<winrt::TerminalApp::implementation::AppKeyBindings>() },
+    _keybindingsWarnings{},
     _colorSchemes{},
     _defaultProfile{},
     _alwaysShowTabs{ true },
@@ -330,7 +331,14 @@ void GlobalAppSettings::LayerJson(const Json::Value& json)
 
     if (auto keybindings{ json[JsonKey(KeybindingsKey)] })
     {
-        _keybindings->LayerJson(keybindings);
+        auto warnings = _keybindings->LayerJson(keybindings);
+        // It's possible that the user provided keybindings have some warnings
+        // in them - problems that we should alert the user to, but we can
+        // recover from. Most of these warnings cannot be detected later in the
+        // Validate settings phase, so we'll collect them now. If there were any
+        // warnings generated from parsing these keybindings, add them to our
+        // list of warnings.
+        _keybindingsWarnings.insert(_keybindingsWarnings.end(), warnings.begin(), warnings.end());
     }
 
     if (auto snapToGridOnResize{ json[JsonKey(SnapToGridOnResizeKey)] })
@@ -536,4 +544,18 @@ void GlobalAppSettings::AddColorScheme(ColorScheme scheme)
 {
     std::wstring name{ scheme.GetName() };
     _colorSchemes[name] = std::move(scheme);
+}
+
+// Method Description:
+// - Return the warnings that we've collected during parsing the JSON for the
+//   keybindings. It's possible that the user provided keybindings have some
+//   warnings in them - problems that we should alert the user to, but we can
+//   recover from.
+// Arguments:
+// - <none>
+// Return Value:
+// - <none>
+std::vector<TerminalApp::SettingsLoadWarnings> GlobalAppSettings::GetKeybindingsWarnings() const
+{
+    return _keybindingsWarnings;
 }

commit 172bca388d9b13f3a37ebdfcf896e3171fddbe66

Fix unbinding keys in v0.10 (#4988)
## Summary of the Pull Request

We (the royal "we") broke key unbinding in #4746. We didn't run the local tests after this, which actually would have caught this. The comment even suggests what we should have done here. We need to make sure that when we bail, it's because there's a parsing function that returned nothing. `null`, `"unbound"`, etc actually don't even have a parsing function at all, so they should just keep on keepin' on.

## References

Source of this regression: #4746

## PR Checklist
* [x] Closes #3729
* [x] I work here
* [x] Tests added/passed
* [n/a] Requires documentation to be updated

## Detailed Description of the Pull Request / Additional comments

This is a great example of why your unittests should run in CI always

## Validation Steps Performed
* **ran the tests**
* tested the following unbindings:

```json
        { "command": null, "keys": [ "ctrl+shift+t" ] },
        { "command": "unbound", "keys": [ "ctrl+shift+t" ] },
        { "command": "null", "keys": [ "ctrl+shift+t" ] },

```
and they each individually worked.

src/cascadia/TerminalApp/AppKeyBindingsSerialization.cpp
@@ -516,7 +516,7 @@ std::vector<::TerminalApp::SettingsLoadWarnings> winrt::TerminalApp::implementat
                 warnings.insert(warnings.end(), parseWarnings.begin(), parseWarnings.end());
 
                 // if an arg parser was registered, but failed, bail
-                if (args == nullptr)
+                if (pfn && args == nullptr)
                 {
                     continue;
                 }