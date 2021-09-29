e82eric/terminal-playground

commit f81ff95af69d35bfbab9585df43b128680aa1f3c

Enable Vintage Opacity (#11180)
## Summary of the Pull Request
![603-final](https://user-images.githubusercontent.com/18356694/132585665-afed3210-257a-4fee-9b43-4273a0f5cf69.gif)

Adds support for vintage style opacity, on Windows 11+. The API we're using for this exists since the time immemorial, but there's a bug in XAML Islands that prevents it from working right until Windows 11 (which we're working on backporting).

Replaces the `acrylicOpacity` setting with `opacity`, which is a uint between 0 and 100 (inclusive), default to 100.

`useAcrylic` now controls whether acrylic is used or not. Setting an opacity < 100 with `"useAcrylic": false` will use vintage style opacity.

Mouse wheeling adjusts opacity. Whether acrylic is used or not is dependent upon `useAcrylic`.

`opacity` will stealthily default to 50 if `useAcrylic:true` is set.

## PR Checklist
* [x] Closes #603
* [x] I work here
* [x] Tests added/passed
* [x] https://github.com/MicrosoftDocs/terminal/pull/416

## Detailed Description of the Pull Request / Additional comments

Opacity was moved to AppearanceConfig. In the future, I have a mind to allow unfocused acrylic, so that'll be important then. 

## Validation Steps Performed
_just look at it_

src/cascadia/TerminalSettingsModel/TerminalSettings.cpp
@@ -201,6 +201,16 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
 
         _IntenseIsBold = WI_IsFlagSet(appearance.IntenseTextStyle(), Microsoft::Terminal::Settings::Model::IntenseStyle::Bold);
         _IntenseIsBright = WI_IsFlagSet(appearance.IntenseTextStyle(), Microsoft::Terminal::Settings::Model::IntenseStyle::Bright);
+
+        // If the user set an opacity, then just use that. Otherwise, change the
+        // default value based off of whether useAcrylic was set or not. If they
+        // want acrylic, then default to 50%. Otherwise, default to 100% (fully
+        // opaque)
+        _Opacity = appearance.HasOpacity() ?
+                       appearance.Opacity() :
+                       UseAcrylic() ?
+                       .5 :
+                       1.0;
     }
 
     // Method Description:
@@ -273,7 +283,6 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
         // Fill in the remaining properties from the profile
         _ProfileName = profile.Name();
         _UseAcrylic = profile.UseAcrylic();
-        _TintOpacity = profile.AcrylicOpacity();
 
         _FontFace = profile.FontInfo().FontFace();
         _FontSize = profile.FontInfo().FontSize();

scratch/ScratchIslandApp/SampleApp/MySettings.h
@@ -45,7 +45,7 @@ namespace winrt::SampleApp::implementation
 
         WINRT_PROPERTY(winrt::hstring, ProfileName);
         WINRT_PROPERTY(bool, UseAcrylic, false);
-        WINRT_PROPERTY(double, TintOpacity, 0.5);
+        WINRT_PROPERTY(double, Opacity, .5);
         WINRT_PROPERTY(winrt::hstring, Padding, DEFAULT_PADDING);
         WINRT_PROPERTY(winrt::hstring, FontFace, L"Consolas");
         WINRT_PROPERTY(int32_t, FontSize, DEFAULT_FONT_SIZE);

src/cascadia/TerminalApp/Pane.cpp
@@ -53,10 +53,6 @@ Pane::Pane(const Profile& profile, const TermControl& control, const bool lastFo
         _SetupResources();
     }
 
-    // Use the unfocused border color as the pane background, so an actual color
-    // appears behind panes as we animate them sliding in.
-    _root.Background(s_unfocusedBorderBrush);
-
     // Register an event with the control to have it inform us when it gains focus.
     _gotFocusRevoker = _control.GotFocus(winrt::auto_revoke, { this, &Pane::_ControlGotFocusHandler });
     _lostFocusRevoker = _control.LostFocus(winrt::auto_revoke, { this, &Pane::_ControlLostFocusHandler });
@@ -1616,6 +1612,8 @@ winrt::fire_and_forget Pane::_CloseChildRoutine(const bool closeFirst)
         // Create the dummy grid. This grid will be the one we actually animate,
         // in the place of the closed pane.
         Controls::Grid dummyGrid;
+        // GH#603 - we can safely add a BG here, as the control is gone right
+        // away, to fill the space as the rest of the pane expands.
         dummyGrid.Background(s_unfocusedBorderBrush);
         // It should be the size of the closed pane.
         dummyGrid.Width(removedOriginalSize.Width);
@@ -1871,6 +1869,19 @@ void Pane::_SetupEntranceAnimation()
         return;
     }
 
+    // Use the unfocused border color as the pane background, so an actual color
+    // appears behind panes as we animate them sliding in.
+    //
+    // GH#603 - We set only the background of the new pane, while it animates
+    // in. Once the animation is done, we'll remove that background, so if the
+    // user wants vintage opacity, they'll be able to see what's under the
+    // window.
+    // * If we don't give it a background, then the BG will be entirely transparent.
+    // * If we give the parent (us) root BG a color, then a transparent pane
+    //   will flash opaque during the animation, then back to transparent, which
+    //   looks bad.
+    _secondChild->_root.Background(s_unfocusedBorderBrush);
+
     const auto [firstSize, secondSize] = _CalcChildrenSizes(::base::saturated_cast<float>(totalSize));
 
     // This is safe to capture this, because it's only being called in the
@@ -1938,11 +1949,12 @@ void Pane::_SetupEntranceAnimation()
 
             // When the animation is completed, undo the trickiness from before, to
             // restore the controls to the behavior they'd usually have.
-            animation.Completed([childGrid, control](auto&&, auto&&) {
+            animation.Completed([childGrid, control, root = _secondChild->_root](auto&&, auto&&) {
                 control.Width(NAN);
                 childGrid.Width(NAN);
                 childGrid.HorizontalAlignment(HorizontalAlignment::Stretch);
                 control.HorizontalAlignment(HorizontalAlignment::Stretch);
+                root.Background(nullptr);
             });
         }
         else
@@ -1956,11 +1968,12 @@ void Pane::_SetupEntranceAnimation()
 
             // When the animation is completed, undo the trickiness from before, to
             // restore the controls to the behavior they'd usually have.
-            animation.Completed([childGrid, control](auto&&, auto&&) {
+            animation.Completed([childGrid, control, root = _secondChild->_root](auto&&, auto&&) {
                 control.Height(NAN);
                 childGrid.Height(NAN);
                 childGrid.VerticalAlignment(VerticalAlignment::Stretch);
                 control.VerticalAlignment(VerticalAlignment::Stretch);
+                root.Background(nullptr);
             });
         }
 

src/cascadia/TerminalApp/TabManagement.cpp
@@ -87,7 +87,7 @@ namespace winrt::TerminalApp::implementation
             TraceLoggingBool(usedManualProfile, "ProfileSpecified", "Whether the new tab specified a profile explicitly"),
             TraceLoggingGuid(profile.Guid(), "ProfileGuid", "The GUID of the profile spawned in the new tab"),
             TraceLoggingBool(settings.DefaultSettings().UseAcrylic(), "UseAcrylic", "The acrylic preference from the settings"),
-            TraceLoggingFloat64(settings.DefaultSettings().TintOpacity(), "TintOpacity", "Opacity preference from the settings"),
+            TraceLoggingFloat64(settings.DefaultSettings().Opacity(), "TintOpacity", "Opacity preference from the settings"),
             TraceLoggingWideString(settings.DefaultSettings().FontFace().c_str(), "FontFace", "Font face chosen in the settings"),
             TraceLoggingWideString(schemeName.data(), "SchemeName", "Color scheme set in the settings"),
             TraceLoggingKeyword(MICROSOFT_KEYWORD_MEASURES),

src/cascadia/TerminalApp/TerminalPage.xaml
@@ -9,10 +9,11 @@
       xmlns:local="using:TerminalApp"
       xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
       xmlns:mux="using:Microsoft.UI.Xaml.Controls"
+      Background="Transparent"
       mc:Ignorable="d">
 
     <Grid x:Name="Root"
-          Background="{ThemeResource ApplicationPageBackgroundThemeBrush}">
+          Background="Transparent">
         <Grid.RowDefinitions>
             <RowDefinition Height="Auto" />
             <RowDefinition Height="*" />

src/cascadia/TerminalControl/ControlCore.cpp
@@ -276,7 +276,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             // GH#5098: Inform the engine of the opacity of the default text background.
             if (_settings.UseAcrylic())
             {
-                _renderEngine->SetDefaultTextBackgroundOpacity(::base::saturated_cast<float>(_settings.TintOpacity()));
+                _renderEngine->SetDefaultTextBackgroundOpacity(::base::saturated_cast<float>(_settings.Opacity()));
             }
 
             THROW_IF_FAILED(_renderEngine->Enable());
@@ -425,41 +425,17 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             return;
         }
 
-        auto newOpacity = std::clamp(_settings.TintOpacity() + adjustment,
+        auto newOpacity = std::clamp(_settings.Opacity() + adjustment,
                                      0.0,
                                      1.0);
-        if (_settings.UseAcrylic())
-        {
-            try
-            {
-                _settings.TintOpacity(newOpacity);
 
-                if (newOpacity >= 1.0)
-                {
-                    _settings.UseAcrylic(false);
-                }
-                else
-                {
-                    // GH#5098: Inform the engine of the new opacity of the default text background.
-                    SetBackgroundOpacity(::base::saturated_cast<float>(newOpacity));
-                }
+        // GH#5098: Inform the engine of the new opacity of the default text background.
+        SetBackgroundOpacity(::base::saturated_cast<float>(newOpacity));
 
-                auto eventArgs = winrt::make_self<TransparencyChangedEventArgs>(newOpacity);
-                _TransparencyChangedHandlers(*this, *eventArgs);
-            }
-            CATCH_LOG();
-        }
-        else if (adjustment < 0)
-        {
-            _settings.UseAcrylic(true);
+        _settings.Opacity(newOpacity);
 
-            //Setting initial opacity set to 1 to ensure smooth transition to acrylic during mouse scroll
-            newOpacity = std::clamp(1.0 + adjustment, 0.0, 1.0);
-            _settings.TintOpacity(newOpacity);
-
-            auto eventArgs = winrt::make_self<TransparencyChangedEventArgs>(newOpacity);
-            _TransparencyChangedHandlers(*this, *eventArgs);
-        }
+        auto eventArgs = winrt::make_self<TransparencyChangedEventArgs>(newOpacity);
+        _TransparencyChangedHandlers(*this, *eventArgs);
     }
 
     void ControlCore::ToggleShaderEffects()

src/cascadia/TerminalControl/IControlAppearance.idl
@@ -13,6 +13,7 @@ namespace Microsoft.Terminal.Control
         Windows.UI.Xaml.VerticalAlignment BackgroundImageVerticalAlignment;
         Boolean IntenseIsBold;
         // IntenseIsBright is in Core Appearance
+        Double Opacity;
 
         // Experimental settings
         Boolean RetroTerminalEffect;

src/cascadia/TerminalControl/IControlSettings.idl
@@ -29,7 +29,6 @@ namespace Microsoft.Terminal.Control
         String ProfileName;
 
         Boolean UseAcrylic;
-        Double TintOpacity;
         ScrollbarState ScrollState;
 
         String FontFace;

commit 89c36c6ec9892315e09d46fb9d00b17005854689

Remove the fallback to 50% opacity when only `useAcrylic` is set (#11363)
This logic was seemingly redundant. There's two cases I'm looking at here:

#### Case 1
```jsonc
    "defaults":
    {
        "opacity": 35
    },
    "list":
    [
        {
            "commandline": "cmd.exe",
            "name": "Command Prompt"
        },
```

In this case, we wouldn't set the `TerminalSettings` Opacity to .35, we'd set it to 1.0, because the profile didn't have an `opactity`.

#### Case 2
```jsonc
    "defaults":
    {
        "useAcrylic": true
    },
    "list":
    [
        {
            "commandline": "cmd.exe",
            "name": "Command Prompt"
        },
```

In this case we still want to have an acrylic effect. Previously, we'd default this effect to 50% opaque. I'm not sure that we can actually get that anymore. BUT it turns out, we _can_ have 100% opacity and HostBackdropAcrylic. It is very subtle, but is maybe something we should be allowing anyways. It kinda looks like:
![image](https://user-images.githubusercontent.com/18356694/135168469-35d1f55b-58d1-4ee3-a717-76000c2574b9.png)



* [x] Fixes #11355
* [x] Regressed in #11180 
* [x] I work here

src/cascadia/TerminalSettingsModel/TerminalSettings.cpp
@@ -202,15 +202,7 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
         _IntenseIsBold = WI_IsFlagSet(appearance.IntenseTextStyle(), Microsoft::Terminal::Settings::Model::IntenseStyle::Bold);
         _IntenseIsBright = WI_IsFlagSet(appearance.IntenseTextStyle(), Microsoft::Terminal::Settings::Model::IntenseStyle::Bright);
 
-        // If the user set an opacity, then just use that. Otherwise, change the
-        // default value based off of whether useAcrylic was set or not. If they
-        // want acrylic, then default to 50%. Otherwise, default to 100% (fully
-        // opaque)
-        _Opacity = appearance.HasOpacity() ?
-                       appearance.Opacity() :
-                       UseAcrylic() ?
-                       .5 :
-                       1.0;
+        _Opacity = appearance.Opacity();
     }
 
     // Method Description: