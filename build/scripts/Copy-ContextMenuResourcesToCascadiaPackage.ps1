e82eric/terminal-playground

commit d05001c8cd180ac8e0a2c40a96bdb72f7a80b6f0

Rename "Windows Terminal" -> "Terminal" (#12264)
This should be most of the surfaces that we really care about for displaying "Windows Terminal". There's a pile of other references in code, but I couldn't find any other resources that mention it. 

I left a lot of the references to Windows Terminal throughout. Seemed like it was fine to keep calling it that in most places, just these localized strings that are going to be displayed in the Shell that should be changed.

Rough compare:
![image](https://user-images.githubusercontent.com/18356694/151248506-edf9a6ab-d93f-438f-8755-cdf6ae643736.png)


The strings were also moved to the Context Menu resources file, because that's localized into more languages. 

@DHowett we may want to spin a full build to make sure this works and I didn't miss anything

* [x] Closes #12091

src/cascadia/CascadiaPackage/Package-Dev.appxmanifest
@@ -20,7 +20,7 @@
     Version="0.0.1.0" />
 
   <Properties>
-    <DisplayName>Windows Terminal Dev</DisplayName>
+    <DisplayName>ms-resource:TerminalApp/ContextMenu/AppNameDev</DisplayName>
     <PublisherDisplayName>A Lone Developer</PublisherDisplayName>
     <Logo>Images\StoreLogo.png</Logo>
   </Properties>
@@ -38,8 +38,8 @@
       Executable="$targetnametoken$.exe"
       EntryPoint="$targetentrypoint$">
       <uap:VisualElements
-        DisplayName="ms-resource:AppNameDev"
-        Description="ms-resource:AppDescriptionDev"
+        DisplayName="ms-resource:TerminalApp/ContextMenu/AppNameDev"
+        Description="ms-resource:TerminalApp/ContextMenu/AppDescriptionDev"
         BackgroundColor="transparent"
         Square150x150Logo="Images\Square150x150Logo.png"
         Square44x44Logo="Images\Square44x44Logo.png">
@@ -47,7 +47,7 @@
           Wide310x150Logo="Images\Wide310x150Logo.png"
           Square71x71Logo="Images\SmallTile.png"
           Square310x310Logo="Images\LargeTile.png"
-          ShortName="ms-resource:AppShortNameDev">
+          ShortName="ms-resource:TerminalApp/ContextMenu/AppShortNameDev">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
@@ -66,7 +66,7 @@
           <uap5:StartupTask
             TaskId="StartTerminalOnLoginTask"
             Enabled="false"
-            DisplayName="ms-resource:AppNameDev" />
+            DisplayName="ms-resource:TerminalApp/ContextMenu/AppNameDev" />
         </uap5:Extension>
         <uap3:Extension Category="windows.appExtensionHost">
               <uap3:AppExtensionHost>
@@ -87,7 +87,7 @@
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.terminal.host"
                 Id="Terminal-Dev"
-                DisplayName="Windows Terminal Dev"
+                DisplayName="Terminal Dev"
                 Description="Terminal host built from microsoft/terminal open source repository"
                 PublicFolder="Public">
                 <uap3:Properties>

src/cascadia/CascadiaPackage/Package-Pre.appxmanifest
@@ -21,7 +21,7 @@
     Version="0.5.0.0" />
 
   <Properties>
-    <DisplayName>Windows Terminal Preview</DisplayName>
+    <DisplayName>ms-resource:TerminalApp/ContextMenu/AppNamePre</DisplayName>
     <PublisherDisplayName>Microsoft Corporation</PublisherDisplayName>
     <Logo>Images\StoreLogo.png</Logo>
   </Properties>
@@ -39,8 +39,8 @@
       Executable="$targetnametoken$.exe"
       EntryPoint="$targetentrypoint$">
       <uap:VisualElements
-        DisplayName="ms-resource:AppNamePre"
-        Description="ms-resource:AppDescriptionPre"
+        DisplayName="ms-resource:TerminalApp/ContextMenu/AppNamePre"
+        Description="ms-resource:TerminalApp/ContextMenu/AppDescriptionPre"
         BackgroundColor="transparent"
         Square150x150Logo="Images\Square150x150Logo.png"
         Square44x44Logo="Images\Square44x44Logo.png">
@@ -48,7 +48,7 @@
           Wide310x150Logo="Images\Wide310x150Logo.png"
           Square71x71Logo="Images\SmallTile.png"
           Square310x310Logo="Images\LargeTile.png"
-          ShortName="ms-resource:AppShortNamePre">
+          ShortName="ms-resource:TerminalApp/ContextMenu/AppShortNamePre">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
@@ -72,7 +72,7 @@
           <uap5:StartupTask
             TaskId="StartTerminalOnLoginTask"
             Enabled="false"
-            DisplayName="ms-resource:AppNamePre" />
+            DisplayName="ms-resource:TerminalApp/ContextMenu/AppNamePre" />
         </uap5:Extension>
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.console.host"
@@ -88,7 +88,7 @@
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.terminal.host"
                 Id="Terminal-Pre"
-                DisplayName="Windows Terminal Preview"
+                DisplayName="Terminal Preview"
                 Description="Terminal host built from microsoft/terminal open source repository"
                 PublicFolder="Public">
                 <uap3:Properties>

src/cascadia/CascadiaPackage/Package.appxmanifest
@@ -21,7 +21,7 @@
     Version="1.0.0.0" />
 
   <Properties>
-    <DisplayName>Windows Terminal</DisplayName>
+    <DisplayName>ms-resource:TerminalApp/ContextMenu/AppName</DisplayName>
     <PublisherDisplayName>Microsoft Corporation</PublisherDisplayName>
     <Logo>Images\StoreLogo.png</Logo>
   </Properties>
@@ -39,8 +39,8 @@
       Executable="$targetnametoken$.exe"
       EntryPoint="$targetentrypoint$">
       <uap:VisualElements
-        DisplayName="ms-resource:AppName"
-        Description="ms-resource:AppDescription"
+        DisplayName="ms-resource:TerminalApp/ContextMenu/AppName"
+        Description="ms-resource:TerminalApp/ContextMenu/AppDescription"
         BackgroundColor="transparent"
         Square150x150Logo="Images\Square150x150Logo.png"
         Square44x44Logo="Images\Square44x44Logo.png">
@@ -48,7 +48,7 @@
           Wide310x150Logo="Images\Wide310x150Logo.png"
           Square71x71Logo="Images\SmallTile.png"
           Square310x310Logo="Images\LargeTile.png"
-          ShortName="ms-resource:AppShortName">
+          ShortName="ms-resource:TerminalApp/ContextMenu/AppShortName">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
@@ -72,7 +72,7 @@
           <uap5:StartupTask
             TaskId="StartTerminalOnLoginTask"
             Enabled="false"
-            DisplayName="ms-resource:AppName" />
+            DisplayName="ms-resource:TerminalApp/ContextMenu/AppName" />
         </uap5:Extension>
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.console.host"
@@ -88,7 +88,7 @@
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.terminal.host"
                 Id="Terminal"
-                DisplayName="Windows Terminal"
+                DisplayName="Terminal"
                 Description="Terminal host built from microsoft/terminal open source repository"
                 PublicFolder="Public">
                 <uap3:Properties>

src/cascadia/CascadiaPackage/Resources/en-US/Resources.resw
@@ -117,13 +117,4 @@
   <resheader name="writer">
     <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </resheader>
-  <data name="AppDescription" xml:space="preserve">
-    <value>The New Windows Terminal</value>
-  </data>
-  <data name="AppDescriptionDev" xml:space="preserve">
-    <value>The Windows Terminal, but Unofficial</value>
-  </data>
-  <data name="AppDescriptionPre" xml:space="preserve">
-    <value>Windows Terminal with a preview of upcoming features</value>
-  </data>
 </root>

src/cascadia/CascadiaPackage/Resources/Resources.resw
@@ -117,22 +117,4 @@
   <resheader name="writer">
     <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </resheader>
-  <data name="AppName" xml:space="preserve">
-    <value>Windows Terminal</value>
-  </data>
-  <data name="AppNameDev" xml:space="preserve">
-    <value>Windows Terminal Dev</value>
-  </data>
-  <data name="AppNamePre" xml:space="preserve">
-    <value>Windows Terminal Preview</value>
-  </data>
-  <data name="AppShortName" xml:space="preserve">
-    <value>Terminal</value>
-  </data>
-  <data name="AppShortNameDev" xml:space="preserve">
-    <value>Terminal Dev</value>
-  </data>
-  <data name="AppShortNamePre" xml:space="preserve">
-    <value>Terminal Preview</value>
-  </data>
 </root>

src/cascadia/TerminalSettingsEditor/Resources/en-US/Resources.resw
@@ -384,7 +384,7 @@
     <comment>Header for a control to toggle whether the terminal's title is shown as the application title, or not.</comment>
   </data>
   <data name="Globals_ShowTitleInTitlebar.HelpText" xml:space="preserve">
-    <value>When disabled, the title bar will be 'Windows Terminal'.</value>
+    <value>When disabled, the title bar will be 'Terminal'.</value>
     <comment>A description for what the "show title in titlebar" setting does. Presented near "Globals_ShowTitleInTitlebar.Header".{Locked="Windows"}</comment>
   </data>
   <data name="Globals_SnapToGridOnResize.Header" xml:space="preserve">
@@ -408,7 +408,7 @@
     <comment>Header for a control to toggle whether the app should launch when the user's machine starts up, or not.</comment>
   </data>
   <data name="Globals_StartOnUserLogin.HelpText" xml:space="preserve">
-    <value>When enabled, this enables the launch of Windows Terminal at machine startup.</value>
+    <value>When enabled, this enables the launch of Terminal at machine startup.</value>
     <comment>A description for what the "start on user login" setting does. Presented near "Globals_StartOnUserLogin.Header".</comment>
   </data>
   <data name="Globals_AlwaysOnTop.Header" xml:space="preserve">
@@ -416,7 +416,7 @@
     <comment>Header for a control to toggle if the app will always be presented on top of other windows, or is treated normally (when disabled).</comment>
   </data>
   <data name="Globals_AlwaysOnTop.HelpText" xml:space="preserve">
-    <value>Windows Terminal will always be the topmost window on the desktop.</value>
+    <value>Terminal will always be the topmost window on the desktop.</value>
     <comment>A description for what the "always on top" setting does. Presented near "Globals_AlwaysOnTop.Header".</comment>
   </data>
   <data name="Globals_TabWidthMode.Header" xml:space="preserve">
@@ -848,7 +848,7 @@
     <comment>A supplementary setting to the "starting directory" setting. "Parent" refers to the parent process of the current process.</comment>
   </data>
   <data name="Profile_StartingDirectoryUseParentCheckbox.[using:Windows.UI.Xaml.Controls]ToolTipService.ToolTip" xml:space="preserve">
-    <value>If enabled, this profile will spawn in the directory from which Windows Terminal was launched.</value>
+    <value>If enabled, this profile will spawn in the directory from which Terminal was launched.</value>
     <comment>A description for what the supplementary "use parent process directory" setting does. Presented near "Profile_StartingDirectoryUseParentCheckbox".</comment>
   </data>
   <data name="Profile_SuppressApplicationTitle.Header" xml:space="preserve">

src/cascadia/TerminalSettingsModel/Resources/en-US/Resources.resw
@@ -127,7 +127,7 @@
     <value>Split Pane...</value>
   </data>
   <data name="ApplicationDisplayNameUnpackaged" xml:space="preserve">
-    <value>Windows Terminal (Unpackaged)</value>
+    <value>Terminal (Unpackaged)</value>
     <comment>This display name is used when the application's name cannot be determined</comment>
   </data>
   <data name="ApplicationVersionUnknown" xml:space="preserve">

src/cascadia/TerminalApp/AppLogic.cpp
@@ -1074,18 +1074,18 @@ namespace winrt::TerminalApp::implementation
 
     // Method Description:
     // - Gets the title of the currently focused terminal control. If there
-    //   isn't a control selected for any reason, returns "Windows Terminal"
+    //   isn't a control selected for any reason, returns "Terminal"
     // Arguments:
     // - <none>
     // Return Value:
-    // - the title of the focused control if there is one, else "Windows Terminal"
+    // - the title of the focused control if there is one, else "Terminal"
     hstring AppLogic::Title()
     {
         if (_root)
         {
             return _root->Title();
         }
-        return { L"Windows Terminal" };
+        return { L"Terminal" };
     }
 
     // Method Description:

commit 458ca74cc4334579f9238974833e78051823bf05

Copy localized strings from ContextMenu into the resource root (#12491)
We chose to use the "ContextMenu" resource compartment when we
changed the package name to Terminal in #12264 because it was more
broadly localized than the rest of the application.

It appears as though some platform features have trouble with the
"more qualified" resource paths that #12264 required.

To fix this, we will:

1. Copy all of the ContextMenu localizations into CascadiaPackage's
   resource root
2. Switch all manifest resource paths to use resources from the package
   root.

Regressed in #12264
Closes #12384
Closes #12406 (tracked in microsoft/powertoys#16118)

build/pipelines/release.yml
@@ -152,6 +152,11 @@ jobs:
         $Files | % { Move-Item -Verbose $_.Directory $_.Directory.Parent.Parent -EA:Ignore }
       pwsh: true
   - task: PowerShell@2
+    displayName: Copy the Context Menu Loc Resources to CascadiaPackage
+    inputs:
+      filePath: ./build/scripts/Copy-ContextMenuResourcesToCascadiaPackage.ps1
+      pwsh: true
+  - task: PowerShell@2
     displayName: Generate NOTICE.html from NOTICE.md
     inputs:
       filePath: .\build\scripts\Generate-ThirdPartyNotices.ps1

build/scripts/Copy-ContextMenuResourcesToCascadiaPackage.ps1
@@ -1,0 +1,34 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+$LocalizationsFromContextMenu = Get-ChildItem ./src/cascadia/TerminalApp/Resources -Recurse -Filter ContextMenu.resw
+$Languages = [System.Collections.HashTable]::New()
+$LocalizationsFromContextMenu | ForEach-Object {
+	$Languages[$_.Directory.Name] = $_
+}
+
+ForEach ($pair in $Languages.GetEnumerator()) {
+	$LanguageDir = "./src/cascadia/CascadiaPackage/Resources/$($pair.Key)"
+	$ResPath = "$LanguageDir/Resources.resw"
+	$PreexistingResw = Get-Item $ResPath -EA:Ignore
+	If ($null -eq $PreexistingResw) {
+		Write-Host "Copying $($pair.Value.FullName) to $ResPath"
+		New-Item -type Directory $LanguageDir -EA:Ignore
+		Copy-Item $pair.Value.FullName $ResPath
+	} Else {
+		# Merge Them!
+		Write-Host "Merging $($pair.Value.FullName) into $ResPath"
+		$existingXml = [xml](Get-Content $PreexistingResw.FullName)
+		$newXml = [xml](Get-Content $pair.Value.FullName)
+		$newDataKeys = $newXml.root.data.name
+		$existingXml.root.data | % {
+			If ($_.name -in $newDataKeys) {
+				$null = $existingXml.root.RemoveChild($_)
+			}
+		}
+		$newXml.root.data | % {
+			$null = $existingXml.root.AppendChild($existingXml.ImportNode($_, $true))
+		}
+		$existingXml.Save($PreexistingResw.FullName)
+	}
+}

src/cascadia/CascadiaPackage/Package-Dev.appxmanifest
@@ -20,7 +20,7 @@
     Version="0.0.1.0" />
 
   <Properties>
-    <DisplayName>ms-resource:TerminalApp/ContextMenu/AppStoreNameDev</DisplayName>
+    <DisplayName>ms-resource:AppStoreNameDev</DisplayName>
     <PublisherDisplayName>A Lone Developer</PublisherDisplayName>
     <Logo>Images\StoreLogo.png</Logo>
   </Properties>
@@ -38,8 +38,8 @@
       Executable="$targetnametoken$.exe"
       EntryPoint="$targetentrypoint$">
       <uap:VisualElements
-        DisplayName="ms-resource:TerminalApp/ContextMenu/AppNameDev"
-        Description="ms-resource:TerminalApp/ContextMenu/AppDescriptionDev"
+        DisplayName="ms-resource:AppNameDev"
+        Description="ms-resource:AppDescriptionDev"
         BackgroundColor="transparent"
         Square150x150Logo="Images\Square150x150Logo.png"
         Square44x44Logo="Images\Square44x44Logo.png">
@@ -47,7 +47,7 @@
           Wide310x150Logo="Images\Wide310x150Logo.png"
           Square71x71Logo="Images\SmallTile.png"
           Square310x310Logo="Images\LargeTile.png"
-          ShortName="ms-resource:TerminalApp/ContextMenu/AppShortNameDev">
+          ShortName="ms-resource:AppShortNameDev">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
@@ -66,7 +66,7 @@
           <uap5:StartupTask
             TaskId="StartTerminalOnLoginTask"
             Enabled="false"
-            DisplayName="ms-resource:TerminalApp/ContextMenu/AppNameDev" />
+            DisplayName="ms-resource:AppNameDev" />
         </uap5:Extension>
         <uap3:Extension Category="windows.appExtensionHost">
               <uap3:AppExtensionHost>

src/cascadia/CascadiaPackage/Package-Pre.appxmanifest
@@ -21,7 +21,7 @@
     Version="0.5.0.0" />
 
   <Properties>
-    <DisplayName>ms-resource:TerminalApp/ContextMenu/AppStoreNamePre</DisplayName>
+    <DisplayName>ms-resource:AppStoreNamePre</DisplayName>
     <PublisherDisplayName>Microsoft Corporation</PublisherDisplayName>
     <Logo>Images\StoreLogo.png</Logo>
   </Properties>
@@ -127,8 +127,8 @@
       Executable="$targetnametoken$.exe"
       EntryPoint="$targetentrypoint$">
       <uap:VisualElements
-        DisplayName="ms-resource:TerminalApp/ContextMenu/AppNamePre"
-        Description="ms-resource:TerminalApp/ContextMenu/AppDescriptionPre"
+        DisplayName="ms-resource:AppNamePre"
+        Description="ms-resource:AppDescriptionPre"
         BackgroundColor="transparent"
         Square150x150Logo="Images\Square150x150Logo.png"
         Square44x44Logo="Images\Square44x44Logo.png">
@@ -136,7 +136,7 @@
           Wide310x150Logo="Images\Wide310x150Logo.png"
           Square71x71Logo="Images\SmallTile.png"
           Square310x310Logo="Images\LargeTile.png"
-          ShortName="ms-resource:TerminalApp/ContextMenu/AppShortNamePre">
+          ShortName="ms-resource:AppShortNamePre">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
@@ -160,7 +160,7 @@
           <uap5:StartupTask
             TaskId="StartTerminalOnLoginTask"
             Enabled="false"
-            DisplayName="ms-resource:TerminalApp/ContextMenu/AppNamePre" />
+            DisplayName="ms-resource:AppNamePre" />
         </uap5:Extension>
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.console.host"

src/cascadia/CascadiaPackage/Package.appxmanifest
@@ -21,7 +21,7 @@
     Version="1.0.0.0" />
 
   <Properties>
-    <DisplayName>ms-resource:TerminalApp/ContextMenu/AppStoreName</DisplayName>
+    <DisplayName>ms-resource:AppStoreName</DisplayName>
     <PublisherDisplayName>Microsoft Corporation</PublisherDisplayName>
     <Logo>Images\StoreLogo.png</Logo>
   </Properties>
@@ -127,8 +127,8 @@
       Executable="$targetnametoken$.exe"
       EntryPoint="$targetentrypoint$">
       <uap:VisualElements
-        DisplayName="ms-resource:TerminalApp/ContextMenu/AppName"
-        Description="ms-resource:TerminalApp/ContextMenu/AppDescription"
+        DisplayName="ms-resource:AppName"
+        Description="ms-resource:AppDescription"
         BackgroundColor="transparent"
         Square150x150Logo="Images\Square150x150Logo.png"
         Square44x44Logo="Images\Square44x44Logo.png">
@@ -136,7 +136,7 @@
           Wide310x150Logo="Images\Wide310x150Logo.png"
           Square71x71Logo="Images\SmallTile.png"
           Square310x310Logo="Images\LargeTile.png"
-          ShortName="ms-resource:TerminalApp/ContextMenu/AppShortName">
+          ShortName="ms-resource:AppShortName">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
@@ -160,7 +160,7 @@
           <uap5:StartupTask
             TaskId="StartTerminalOnLoginTask"
             Enabled="false"
-            DisplayName="ms-resource:TerminalApp/ContextMenu/AppName" />
+            DisplayName="ms-resource:AppName" />
         </uap5:Extension>
         <uap3:Extension Category="windows.appExtension">
             <uap3:AppExtension Name="com.microsoft.windows.console.host"

src/cascadia/CascadiaPackage/Resources/en-US/Resources.resw
@@ -117,4 +117,52 @@
   <resheader name="writer">
     <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </resheader>
+  <data name="AppName" xml:space="preserve">
+    <value>Terminal</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppNameDev" xml:space="preserve">
+    <value>Terminal Dev</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppNamePre" xml:space="preserve">
+    <value>Terminal Preview</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppStoreName" xml:space="preserve">
+    <value>Windows Terminal</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppStoreNameDev" xml:space="preserve">
+    <value>Windows Terminal Dev</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppStoreNamePre" xml:space="preserve">
+    <value>Windows Terminal Preview</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppShortName" xml:space="preserve">
+    <value>Terminal</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppShortNameDev" xml:space="preserve">
+    <value>Terminal Dev</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppShortNamePre" xml:space="preserve">
+    <value>Terminal Preview</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppDescription" xml:space="preserve">
+    <value>The New Windows Terminal</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppDescriptionDev" xml:space="preserve">
+    <value>The Windows Terminal, but Unofficial</value>
+    <comment>{Locked}</comment>
+  </data>
+  <data name="AppDescriptionPre" xml:space="preserve">
+    <value>Windows Terminal with a preview of upcoming features</value>
+    <comment>{Locked}</comment>
+  </data>
 </root>