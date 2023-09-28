e82eric/terminal-playground

commit 4cf9fcd4fca770483699549a8b29f0be0946cf91

Option to default to show icon in tab, hide tabicon or make icon in tab monochrome. (#15948)
Adding enum iconstyle for hiding the icon in the tab #8157 

## Summary of the Pull Request
Please confirm if I am on the right track.
## References and Relevant Issues

## Detailed Description of the Pull Request / Additional comments

## Validation Steps Performed

## PR Checklist
- [ ] Closes #8157
- [x] Tests added/passed
- [ ] Documentation updated
- If checked, please file a pull request on [our docs
repo](https://github.com/MicrosoftDocs/terminal) and link it here: #xxx
- [ ] Schema updated (if necessary)

src/cascadia/TerminalApp/TabManagement.cpp
@@ -176,7 +176,7 @@ namespace winrt::TerminalApp::implementation
         {
             if (!profile.Icon().empty())
             {
-                newTabImpl->UpdateIcon(profile.Icon());
+                newTabImpl->UpdateIcon(profile.Icon(), _settings.GlobalSettings().CurrentTheme().Tab().IconStyle());
             }
         }
 
@@ -241,7 +241,7 @@ namespace winrt::TerminalApp::implementation
     {
         if (const auto profile = tab.GetFocusedProfile())
         {
-            tab.UpdateIcon(profile.Icon());
+            tab.UpdateIcon(profile.Icon(), _settings.GlobalSettings().CurrentTheme().Tab().IconStyle());
         }
     }
 

src/cascadia/LocalTests_SettingsModel/ThemeTests.cpp
@@ -60,7 +60,8 @@ namespace SettingsModelLocalTests
             "tabRow":
             {
                 "background": "#FFFF8800",
-                "unfocusedBackground": "#FF8844"
+                "unfocusedBackground": "#FF8844",
+                "iconStyle": "default"
             },
             "window":
             {

src/cascadia/TerminalApp/SettingsTab.cpp
@@ -114,7 +114,7 @@ namespace winrt::TerminalApp::implementation
 
         // The TabViewItem Icon needs MUX while the IconSourceElement in the CommandPalette needs WUX...
         Icon(winrt::hstring{ glyph });
-        TabViewItem().IconSource(IconPathConverter::IconSourceMUX(glyph));
+        TabViewItem().IconSource(IconPathConverter::IconSourceMUX(glyph, false));
     }
 
     winrt::Windows::UI::Xaml::Media::Brush SettingsTab::_BackgroundBrush()

src/cascadia/TerminalApp/TerminalTab.cpp
@@ -280,17 +280,17 @@ namespace winrt::TerminalApp::implementation
     // - iconPath: The new path string to use as the IconPath for our TabViewItem
     // Return Value:
     // - <none>
-    void TerminalTab::UpdateIcon(const winrt::hstring iconPath)
+    void TerminalTab::UpdateIcon(const winrt::hstring iconPath, const winrt::Microsoft::Terminal::Settings::Model::IconStyle iconStyle)
     {
         ASSERT_UI_THREAD();
 
-        // Don't reload our icon if it hasn't changed.
-        if (iconPath == _lastIconPath)
+        // Don't reload our icon and iconStyle hasn't changed.
+        if (iconPath == _lastIconPath && iconStyle == _lastIconStyle)
         {
             return;
         }
-
         _lastIconPath = iconPath;
+        _lastIconStyle = iconStyle;
 
         // If the icon is currently hidden, just return here (but only after setting _lastIconPath to the new path
         // for when we show the icon again)
@@ -299,9 +299,18 @@ namespace winrt::TerminalApp::implementation
             return;
         }
 
-        // The TabViewItem Icon needs MUX while the IconSourceElement in the CommandPalette needs WUX...
-        Icon(_lastIconPath);
-        TabViewItem().IconSource(IconPathConverter::IconSourceMUX(_lastIconPath));
+        if (iconStyle == IconStyle::Hidden)
+        {
+            // The TabViewItem Icon needs MUX while the IconSourceElement in the CommandPalette needs WUX...
+            Icon({});
+            TabViewItem().IconSource(IconSource{ nullptr });
+        }
+        else
+        {
+            Icon(_lastIconPath);
+            bool isMonochrome = iconStyle == IconStyle::Monochrome;
+            TabViewItem().IconSource(IconPathConverter::IconSourceMUX(_lastIconPath, isMonochrome));
+        }
     }
 
     // Method Description:
@@ -323,7 +332,7 @@ namespace winrt::TerminalApp::implementation
             else
             {
                 Icon(_lastIconPath);
-                TabViewItem().IconSource(IconPathConverter::IconSourceMUX(_lastIconPath));
+                TabViewItem().IconSource(IconPathConverter::IconSourceMUX(_lastIconPath, _lastIconStyle == IconStyle::Monochrome));
             }
             _iconHidden = hide;
         }

src/cascadia/TerminalApp/TerminalTab.h
@@ -41,7 +41,7 @@ namespace winrt::TerminalApp::implementation
                        std::shared_ptr<Pane> newPane);
 
         void ToggleSplitOrientation();
-        void UpdateIcon(const winrt::hstring iconPath);
+        void UpdateIcon(const winrt::hstring iconPath, const winrt::Microsoft::Terminal::Settings::Model::IconStyle iconStyle);
         void HideIcon(const bool hide);
 
         void ShowBellIndicator(const bool show);
@@ -111,6 +111,7 @@ namespace winrt::TerminalApp::implementation
         Windows::UI::Xaml::Controls::MenuFlyoutItem _closePaneMenuItem;
         Windows::UI::Xaml::Controls::MenuFlyoutItem _restartConnectionMenuItem;
 
+        winrt::Microsoft::Terminal::Settings::Model::IconStyle _lastIconStyle;
         winrt::hstring _lastIconPath{};
         std::optional<winrt::Windows::UI::Color> _runtimeTabColor{};
         winrt::TerminalApp::TabHeaderControl _headerControl{};

src/cascadia/TerminalSettingsModel/IconPathConverter.cpp
@@ -72,7 +72,7 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
     // Return Value:
     // - An IconElement with its IconSource set, if possible.
     template<typename TIconSource>
-    TIconSource _getColoredBitmapIcon(const winrt::hstring& path)
+    TIconSource _getColoredBitmapIcon(const winrt::hstring& path, bool monochrome)
     {
         // FontIcon uses glyphs in the private use area, whereas valid URIs only contain ASCII characters.
         // To skip throwing on Uri construction, we can quickly check if the first character is ASCII.
@@ -85,7 +85,7 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
                 // Make sure to set this to false, so we keep the RGB data of the
                 // image. Otherwise, the icon will be white for all the
                 // non-transparent pixels in the image.
-                iconSource.ShowAsMonochrome(false);
+                iconSource.ShowAsMonochrome(monochrome);
                 iconSource.UriSource(iconUri);
                 return iconSource;
             }
@@ -121,14 +121,14 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
     // Return Value:
     // - An IconElement with its IconSource set, if possible.
     template<typename TIconSource>
-    TIconSource _getIconSource(const winrt::hstring& iconPath)
+    TIconSource _getIconSource(const winrt::hstring& iconPath, bool monochrome)
     {
         TIconSource iconSource{ nullptr };
 
         if (iconPath.size() != 0)
         {
             const auto expandedIconPath{ _expandIconPath(iconPath) };
-            iconSource = _getColoredBitmapIcon<TIconSource>(expandedIconPath);
+            iconSource = _getColoredBitmapIcon<TIconSource>(expandedIconPath, monochrome);
 
             // If we fail to set the icon source using the "icon" as a path,
             // let's try it as a symbol/emoji.
@@ -197,7 +197,7 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
                                                         const hstring& /* language */)
     {
         const auto& iconPath = winrt::unbox_value_or<winrt::hstring>(value, L"");
-        return _getIconSource<Controls::IconSource>(iconPath);
+        return _getIconSource<Controls::IconSource>(iconPath, false);
     }
 
     // unused for one-way bindings
@@ -211,12 +211,12 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
 
     Windows::UI::Xaml::Controls::IconSource _IconSourceWUX(hstring path)
     {
-        return _getIconSource<Windows::UI::Xaml::Controls::IconSource>(path);
+        return _getIconSource<Windows::UI::Xaml::Controls::IconSource>(path, false);
     }
 
-    Microsoft::UI::Xaml::Controls::IconSource _IconSourceMUX(hstring path)
+    Microsoft::UI::Xaml::Controls::IconSource _IconSourceMUX(hstring path, bool monochrome)
     {
-        return _getIconSource<Microsoft::UI::Xaml::Controls::IconSource>(path);
+        return _getIconSource<Microsoft::UI::Xaml::Controls::IconSource>(path, monochrome);
     }
 
     SoftwareBitmap _convertToSoftwareBitmap(HICON hicon,
@@ -329,13 +329,14 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
         return bitmapSource;
     }
 
-    MUX::Controls::IconSource IconPathConverter::IconSourceMUX(const winrt::hstring& iconPath)
+    MUX::Controls::IconSource IconPathConverter::IconSourceMUX(const winrt::hstring& iconPath,
+                                                               const bool monochrome)
     {
         std::wstring_view iconPathWithoutIndex;
         const auto indexOpt = _getIconIndex(iconPath, iconPathWithoutIndex);
         if (!indexOpt.has_value())
         {
-            return _IconSourceMUX(iconPath);
+            return _IconSourceMUX(iconPath, monochrome);
         }
 
         const auto bitmapSource = _getImageIconSourceForBinary(iconPathWithoutIndex, indexOpt.value());

src/cascadia/TerminalSettingsModel/IconPathConverter.h
@@ -19,7 +19,7 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
                                                       const hstring& language);
 
         static Windows::UI::Xaml::Controls::IconElement IconWUX(const winrt::hstring& iconPath);
-        static Microsoft::UI::Xaml::Controls::IconSource IconSourceMUX(const winrt::hstring& iconPath);
+        static Microsoft::UI::Xaml::Controls::IconSource IconSourceMUX(const winrt::hstring& iconPath, const bool convertToGrayscale);
     };
 }
 

src/cascadia/TerminalSettingsModel/IconPathConverter.idl
@@ -16,7 +16,7 @@ namespace Microsoft.Terminal.Settings.Model
         IconPathConverter();
 
         static Windows.UI.Xaml.Controls.IconElement IconWUX(String path);
-        static Microsoft.UI.Xaml.Controls.IconSource IconSourceMUX(String path);
+        static Microsoft.UI.Xaml.Controls.IconSource IconSourceMUX(String path, Boolean convertToGrayscale);
     };
 
 }

commit 1b05479d52e7c14fca8f9c36fadfb9b8bd041187

Fix a crash for users without a `tab` theme (#16046)
One day into 1.19, and there's a LOT of hits here (**76.25%** of our
~300 crashes). A crash if the Theme doesn't have a `tab` member.

Regressed in #15948

Closes MSFT:46714723

src/cascadia/TerminalApp/TabManagement.cpp
@@ -176,7 +176,9 @@ namespace winrt::TerminalApp::implementation
         {
             if (!profile.Icon().empty())
             {
-                newTabImpl->UpdateIcon(profile.Icon(), _settings.GlobalSettings().CurrentTheme().Tab().IconStyle());
+                const auto theme = _settings.GlobalSettings().CurrentTheme();
+                const auto iconStyle = (theme && theme.Tab()) ? theme.Tab().IconStyle() : IconStyle::Default;
+                newTabImpl->UpdateIcon(profile.Icon(), iconStyle);
             }
         }
 
@@ -241,7 +243,9 @@ namespace winrt::TerminalApp::implementation
     {
         if (const auto profile = tab.GetFocusedProfile())
         {
-            tab.UpdateIcon(profile.Icon(), _settings.GlobalSettings().CurrentTheme().Tab().IconStyle());
+            const auto theme = _settings.GlobalSettings().CurrentTheme();
+            const auto iconStyle = (theme && theme.Tab()) ? theme.Tab().IconStyle() : IconStyle::Default;
+            tab.UpdateIcon(profile.Icon(), iconStyle);
         }
     }
 